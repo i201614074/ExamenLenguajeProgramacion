b55a677eb8db6abf43bba7edee8d4ee7b97c1e00
unknown <i201614074@cibertec.edu.pe>
2024-02-09 21:25:24 -0500

    update

--- src/main/java/daos/SubjectDao.java
+++ src/main/java/daos/SubjectDao.java
@@ -1,5 +1,21 @@
 package daos;
 
+import java.util.List;
+import model.Subject;
+
 public interface SubjectDao {
+	
+	public void create (Subject subject);
+	
+	public void update (Subject subject);
+	
+	public void delete (Integer id);
+	
+	public Subject find (Integer id);
+	
+	public List<Subject> findAll();
+	
+	
+	
 
 }
